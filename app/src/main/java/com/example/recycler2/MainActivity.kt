12655850a005c81fdc2d939bcc838eda38ccebcd
EAG <mataramosmiguel@gmail.com>
2024-01-16 19:00:50 +0100

    no se pone horizontal el conchasumare

--- app/src/main/java/com/example/recycler2/MainActivity.kt
+++ app/src/main/java/com/example/recycler2/MainActivity.kt
@@ -2,10 +2,31 @@ package com.example.recycler2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.recycler2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var bind : ActivityMainBinding
+    private lateinit var adaptadorficha : AdaptadorFicha
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var data = mutableListOf(
+            Ficha("Rey",getDrawable(R.drawable.rey)!!),
+            Ficha("Dama",getDrawable(R.drawable.dama)!!),
+            Ficha("Torre",getDrawable(R.drawable.torre)!!),
+            Ficha("Alfil",getDrawable(R.drawable.alfil)!!),
+            Ficha("Caballo",getDrawable(R.drawable.caballo)!!),
+            Ficha("Peon",getDrawable(R.drawable.peon)!!)
+        )
+
+        adaptadorficha = AdaptadorFicha(data)
+
+        bind.recycler.apply {
+            layoutManager = LinearLayoutManager(applicationContext, LinearLayoutManager.HORIZONTAL, false)
+            adapter = adaptadorficha
+        }
+
     }
 }
